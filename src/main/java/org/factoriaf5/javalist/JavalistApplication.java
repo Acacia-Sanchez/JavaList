48374fcb215d26c320e71827a4c565bb1319485c
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-10-21 23:32:44 +0200

    created java file for methods, with structure and the first method & only file and structure for testing java

--- src/main/java/org/factoriaf5/javalist/JavalistApplication.java
+++ src/main/java/org/factoriaf5/javalist/JavalistApplication.java
@@ -1,13 +1,63 @@
 package org.factoriaf5.javalist;
 
-import org.springframework.boot.SpringApplication;
+import java.util.List;
+import java.util.ArrayList; 
+import java.util.Arrays;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavalistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavalistApplication.class, args);
+	// 1. método para crear la lista de los días de la semana
+	public List<String> creaListaDiasSemana() {
+		List<String> listadias = new ArrayList<>(Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
+/* 		listadias.add("Lunes");
+		listadias.add("Martes");
+		listadias.add("Miércoles");
+		listadias.add("Jueves");
+		listadias.add("Viernes");
+		listadias.add("Sábado");
+		listadias.add("Domingo"); */
+		
+		return listadias;
+	}
+
+	// 2. método que retorne los días de la semana
+	public String muestraDiasSemana(String dia) {
+		return null;
+	}
+
+	// 3. método que retorne el largo de la lista
+	public String muestraLongitudLista(String dia) {
+		return null;
+	}
+
+	// 4. método para eliminar un día de la semana
+	public String eliminarDiaSemana(String dia) {
+		return null;
+	}
+
+	// 5. método que retorne el día de la semana solicitado
+	public String muestraDiaSemana(String dia) {
+		return null;
+	}
+
+	// 6. método que retorne si el día solicitado existe en la lista
+	public String existeDiaSemana(String dia) {
+		return null;
+	}
+
+	// 7. método para ordenar la lista de días por orden alfabético
+	public String ordenDiasSemana(String dia) {
+		return null;
+	}
+
+	// 8. método para vaciar la lista
+	public String vaciaListaDiasSemana(String dia) {
+		return null;
 	}
 
 }
+
+/// HACERLO CON Y SIN GETTERS Y SETTERS ****************
--- src/test/java/org/factoriaf5/javalist/JavalistApplicationTests.java
+++ src/test/java/org/factoriaf5/javalist/JavalistApplicationTests.java
@@ -1,13 +1,59 @@
 package org.factoriaf5.javalist;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavalistApplicationTests {
 
+	// instanciación una sola vez para todos los tests
+	private JavalistApplication javalist;
+
+	@BeforeEach
+	public void init() {
+		this.javalist = new JavalistApplication();
+	}
+
 	@Test
-	void contextLoads() {
+	// 1. método para crear la lista de los días de la semana
+	public void TestCreaListaDiasSemana(String listadias) {
+		List<String>
+	}
+
+	// 2. método que retorne los días de la semana
+	public void TestMuestraDiasSemana(String dia) {
+
+	}
+
+	// 3. método que retorne el largo de la lista
+	public void TestMuestraLongitudLista(String dia) {
+
+	}
+
+	// 4. método para eliminar un día de la semana
+	public void TestEliminarDiaSemana(String dia) {
+
+	}
+
+	// 5. método que retorne el día de la semana solicitado
+	public void TestMuestraDiaSemana(String dia) {
+
+	}
+
+	// 6. método que retorne si el día solicitado existe en la lista
+	public void TestExisteDiaSemana(String dia) {
+
+	}
+
+	// 7. método para ordenar la lista de días por orden alfabético
+	public void TestOrdenDiasSemana(String dia) {
+
+	}
+
+	// 8. método para vaciar la lista
+	public void TestVaciaListaDiasSemana(String dia) {
+
 	}
 
 }
