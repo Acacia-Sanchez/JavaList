e931083590fba3269c4a63d5f26dfd0a9f334f6b
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-10-25 20:04:41 +0200

    terminados metodos y tests, aunque al 92% pq no me dio tiempo a terminar el metodo 6

--- src/main/java/org/factoriaf5/javalist/JavalistApplication.java
+++ src/main/java/org/factoriaf5/javalist/JavalistApplication.java
@@ -1,63 +1,58 @@
 package org.factoriaf5.javalist;
 
-import java.util.List;
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+// @SpringBootApplication
 public class JavalistApplication {
 
-	private List<String> listadias; // definida como atributo de clase para poder usarla en varios métodos
+	private List<String> miListaDias; // definida como atributo de clase para poder usarla en varios métodos
 
 	// 1. método para crear la lista de los días de la semana
-	public void creaListaDiasSemana(List<String> listadias) {
-		listadias.addAll(Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
-/* 		listadias.add("Lunes");
-		listadias.add("Martes");
-		listadias.add("Miércoles");  OTRA FORMA DE HACERLO
-		listadias.add("Jueves");
-		listadias.add("Viernes");
-		listadias.add("Sábado");
-		listadias.add("Domingo"); */
+	public void creaListaDiasSemana() {
+		// miListaDias.addAll(Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
+		miListaDias = new ArrayList<>(Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
 	}
 
 	// 2. método que retorne los días de la semana
 	public List<String> muestraDiasSemana() {
-		return listadias;
+		return miListaDias;
 	}
 
 	// 3. método que retorne el largo de la lista
-	public String muestraLongitudLista(String dia) {
-		return null;
+	public Integer muestraLongitudLista() {
+		return miListaDias.size();
 	}
 
 	// 4. método para eliminar un día de la semana
-	public String eliminarDiaSemana(String dia) {
-		return null;
+	public boolean eliminarDiaSemana(String dia) {
+		return miListaDias.remove(dia);
 	}
 
 	// 5. método que retorne el día de la semana solicitado
 	public String muestraDiaSemana(String dia) {
-		return null;
+		return miListaDias.get(miListaDias.indexOf(dia));
 	}
 
 	// 6. método que retorne si el día solicitado existe en la lista
 	public String existeDiaSemana(String dia) {
-		return null;
+		if (miListaDias.contains(dia)) {
+			return "El día " + dia + " existe";
+		} else {
+			return "El día " + dia + " no existe";
+		}
 	}
 
 	// 7. método para ordenar la lista de días por orden alfabético
-	public String ordenDiasSemana(String dia) {
-		return null;
+	public void ordenDiasSemana() {
+		Collections.sort(miListaDias); // NECESITÉ IMPORTAR COLLECTIONS Y NO ME LO DIJO EN QUICK-FIX.... **** ¿??¿?¿?
 	}
 
 	// 8. método para vaciar la lista
-	public String vaciaListaDiasSemana(String dia) {
-		return null;
+	public void vaciaListaDiasSemana() {
+		miListaDias.clear();
 	}
 
 }
-
-/// HACERLO CON Y SIN GETTERS Y SETTERS ****************
--- src/test/java/org/factoriaf5/javalist/JavalistApplicationTests.java
+++ src/test/java/org/factoriaf5/javalist/JavalistApplicationTests.java
@@ -1,76 +1,110 @@
 package org.factoriaf5.javalist;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-
-@SpringBootTest
+// @SpringBootTest
 class JavalistApplicationTests {
 
 	// instanciación una sola vez para todos los tests
-	private JavalistApplication javalist;
+	private JavalistApplication javalist;  // ES NECESARIO PONER PRIVATE O SE PUEDE QUITAR ??
 
 	@BeforeEach
 	public void init() {
-		this.javalist = new JavalistApplication();
+		javalist = new JavalistApplication(); 
+		javalist.creaListaDiasSemana();
 	}
 
-		// 1. test método crear la lista de los días de la semana
+ 		// 1. test método crear la lista de los días de la semana
 	@Test
 	public void TestCreaListaDiasSemana() {
-		List<String> listadiasesperados = Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-		List<String> listadias = new ArrayList<>();
-		javalist.creaListaDiasSemana(listadias);
-		assertEquals(listadiasesperados, listadias);
-	}
+		List<String> listaTest = Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+		List<String> miListaDias = javalist.muestraDiasSemana();
+
+		assertEquals(listaTest, miListaDias);
+	} 
 
 		// 2. test método que retorne los días de la semana
 	@Test
-	public void TestMuestraDiasSemana(String dia) {
+	public void testMuestraDiasSemana() {
+		List<String> listaTest = Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+		List<String> miListaDias = javalist.muestraDiasSemana();
 
+		assertEquals(listaTest, miListaDias);
 	}
 
 		// 3. test método que retorne el largo de la lista
 	@Test
-	public void TestMuestraLongitudLista(String dia) {
-
+	public void testMuestraLongitudLista() {
+		Integer largoLista = 7;
+		Integer result = javalist.muestraLongitudLista();
+		assertEquals(largoLista, result);
 	}
 
 		// 4. test método para eliminar un día de la semana
 	@Test
-	public void TestEliminarDiaSemana(String dia) {
+	public void testEliminarDiaSemana() {
+		String dia = "Martes";
+		Boolean result = javalist.eliminarDiaSemana(dia);
 
+		assertTrue(result);
 	}
 
 		// 5. test método que retorne el día de la semana solicitado
 	@Test
-	public void TestMuestraDiaSemana(String dia) {
+	public void testMuestraDiaSemana() {
+		String diaSemana = "Viernes";
+		String result = javalist.muestraDiaSemana(diaSemana);
 
+		assertEquals(diaSemana, result);
+	}	
+
+		// 6a. test método que retorne si el día solicitado existe en la lista
+  	@Test
+	public void testExisteDiaSemana() {
+		String diaAComprobar = "Martes";
+		String siExiste = "El día " + diaAComprobar + " existe";
+		String comprobacion = javalist.existeDiaSemana(diaAComprobar);
+
+		assertEquals(siExiste, comprobacion);
+	}
+/* 	// 6b. test método que retorne si el día solicitado NO existe en la lista
+	@Test
+	public void testNoExisteDiaSemana() {
+		javalist.vaciaListaDiasSemana();      ***** ME FALLA EL TEST Y NO ME DA TIEMPO A DAR CON EL ERROR, ASÍ QUE NO LLEGO AL 100% ************
+    	javalist.creaListaDiasSemana();
+		String diaAComprobar = "Lunes";
+		String noExiste = "El día " + diaAComprobar + " no existe";
+		String comprobacion = javalist.existeDiaSemana(diaAComprobar);
+
+		assertEquals(noExiste, comprobacion);
+	}  */
+
+	// 7. test método para ordenar la lista de días por orden alfabético
+	@Test
+	public void testOrdenDiasSemana() {
+		List<String> listaDesordenada = Arrays.asList("Viernes", "Lunes", "Miércoles", "Jueves", "Martes", "Domingo", "Sábado");
+		javalist.ordenDiasSemana();
+		List<String> ordenaLaLista = javalist.muestraDiasSemana();
+		List<String> listaOrdenada = Arrays.asList("Domingo", "Jueves", "Lunes", "Martes", "Miércoles", "Sábado", "Viernes"); // Lista ordenada alfabéticamente
+
+		assertEquals(ordenaLaLista, listaOrdenada);
 	}
 
-		// 6. test método que retorne si el día solicitado existe en la lista
+	// 8. test método para vaciar la lista
 	@Test
-	public void TestExisteDiaSemana(String dia) {
+	public void testVaciaListaDiasSemana() {
+		javalist.vaciaListaDiasSemana();
+		List<String> listVacia = javalist.muestraDiasSemana();
 
-	}
-
-		// 7. test método para ordenar la lista de días por orden alfabético
-	@Test
-	public void TestOrdenDiasSemana(String dia) {
-
-	}
-
-		// 8. test método para vaciar la lista
-	@Test
-	public void TestVaciaListaDiasSemana(String dia) {
-
+        assertTrue(listVacia.isEmpty());
 	}
 
 }
